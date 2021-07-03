cf6293a3ae527578c288f5b2d1d551e064e639b5
yasaman <45336465+yasmansh@users.noreply.github.com>
2021-07-03 20:04:44 +0430

    bugs fix and do_part func

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-from copy import deepcopy
-
 import numpy as np
 from queue import PriorityQueue
 
@@ -8,15 +6,16 @@ def get_arrival():
     return round(-np.log(1 - np.random.uniform(low=0.0, high=1.0)) / landa, 3)
 
 
-def get_service_time():
-    k = round(-np.log(1 - np.random.uniform(low=0.0, high=1.0)) / m, 3)
-    print("service time" + str(k))
+def get_service_time(miu):
+    k = round(-np.log(1 - np.random.uniform(low=0.0, high=1.0)) / miu, 3)
+    print("line12-service time" + str(k))
     return k
 
 
 def get_next():
-    k = (get_level(), current_time + get_arrival(), get_leave_time())
-    return k
+    n = (get_level(), current_time + get_arrival(), get_leave_time())
+    print("line17-get next func= ", n)
+    return n
 
 
 def get_part():
@@ -27,7 +26,7 @@ def get_part():
 
 
 def get_leave_time():
-    l = -np.log(1 - np.random.uniform(low=0.0, high=1.0)) / a
+    l = -np.log(1 - np.random.uniform(low=0.0, high=1.0)) / alpha
     return round(l, 3)
 
 
@@ -46,14 +45,17 @@ def get_level():
 
 
 def do_part(part_number, waited_time, element):
-    pass
+    print("line48 part = ", part_number)
+    new_element = (element[0], (element[1], element[2], waited_time))  # priority, arrival_time, lave_time, waited_time
+    print_parts[part_number].append(new_element)
+    pq_parts[part_number].put(new_element)
 
 
 class Queue:
     def __init__(self, means, distribution):
         self.distribution = distribution
         self.clerk_number = len(means)
-        self.q = PriorityQueue()
+        self.priority_queue = PriorityQueue()
         self.means = means
         self.number_in_queue = 0
         self.wait_time_in_queue = 0
@@ -66,12 +68,12 @@ class Queue:
         self.print_queue = []
 
     def add_queue(self, element):
-        l = (element[0], (element[1], element[2]))
-        self.q.put(l)
-        self.print_queue.append(l)
+        new_element = (element[0], (element[1], element[2]))
+        self.priority_queue.put(new_element)
+        self.print_queue.append(new_element)
 
     def get_queue(self):
-        b = self.q.get()
+        b = self.priority_queue.get()
         return (b[0], b[1][0], b[1][1])
 
     def delete(self, a):
@@ -83,32 +85,37 @@ class Queue:
             print(i)
 
 
+total_number = 0
+max_number = 10  ##10^7 bayad beshe
+current_time = 0
 inputs = input().split(',')
 n = int(inputs[0])
 landa = float(inputs[1])
-m = float(inputs[2])
-a = float(inputs[3])
-m_staffs = []
+miu = float(inputs[2])
+alpha = float(inputs[3])
+
+pq_parts = []
+print_parts = []
 for i in range(0, n):
-    inputs = input().split(',')
+    pq_parts.append(PriorityQueue())
+    print_parts.append([])
+
+miu_staffs = []
+for i in range(0, n):
+    inputs = [*map(float, input().split())]
     number_of_staff = len(inputs)
-    staffs = []
-    for j in range(0, number_of_staff):
-        staffs.append(float(inputs[j]))
-    m_staffs.append(staffs)
+    miu_staffs.append(inputs)
 
 reception_state = False
-current_time = 0
 reception_departure = float('inf')
 next_reception_arrival = get_next()
-reception_q = Queue([m], "poison")
-parts = []
-for i in range(0, n):
-    parts.append(Queue(m_staffs[i], "poison"))
+total_number += 1
+reception_q = Queue([miu], "poisson")
 
-while True:
+while total_number < max_number:
     reception_q.print_q()
     t = min(next_reception_arrival[1], reception_departure)
+    print("line117\nline119 t = ", t)
     reception_q.wait_time_in_queue += (reception_q.number_in_queue * (t - current_time))
     current_time = t
 
@@ -116,17 +123,17 @@ while True:
         reception_q.arrivals += 1
         reception_q.number_in_system += 1
 
-        if reception_q.number_in_queue == 0 and reception_state == False:  # reception is idle
-            reception_service_time = get_service_time()
+        if reception_q.number_in_queue == 0 and not reception_state:  # reception is idle
+            reception_service_time = get_service_time(miu)
             if next_reception_arrival[2] + next_reception_arrival[1] > current_time + reception_service_time:
                 reception_q.service_time += reception_service_time
                 reception_departure = current_time + reception_service_time
-                p = get_part()
-                do_part(p, 0, next_reception_arrival)
+                part_number = get_part()
+                do_part(part_number, 0, next_reception_arrival)
                 reception_state = True
-                print("serviced")
-                print(next_reception_arrival)
+                print("line 135 - serviced", next_reception_arrival)
                 next_reception_arrival = get_next()
+                total_number += 1
             else:
                 reception_q.left_person += 1
 
@@ -143,20 +150,21 @@ while True:
             reception_q.number_in_queue += 1
             reception_q.waited_number += 1
             next_reception_arrival = get_next()
+            total_number += 1
     else:
         reception_q.serviced_person_number += 1
         if reception_q.number_in_queue > 0:
             w = reception_q.get_queue()
-            reception_service_time = get_service_time()
+            reception_service_time = get_service_time(miu)
             if current_time + reception_service_time < w[2] + w[1]:
-                print("serviced")
-                print(w)
+                print("line 161 - serviced ", w)
+                part_number = get_part()
+                do_part(part_number, current_time + reception_service_time - w[1], w)  # check she parameter2sh
                 reception_q.service_time += reception_service_time
                 reception_departure = current_time + reception_service_time
                 reception_q.number_in_queue -= 1
             else:
-                print("left2")
-                print(w)
+                print("line168- left2\t", w)
                 reception_q.number_in_queue -= 1
                 reception_q.left_person += 1
         else:
@@ -165,3 +173,10 @@ while True:
 # what is the staff distribution
 # how people choose parts
 # termination condition
+print("total number = ", max_number)
+print("Number of leavers = ", reception_q.left_person)
+print("~~start parts~~")
+num_parts = []
+for i in range(0, n):
+    num_parts.append(len(print_parts[i]))
+print(num_parts)
